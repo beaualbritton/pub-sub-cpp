fddf934d3152b34f7d3de4a9bb9caf56f095f6f1
beau albritton <albrittoncb@gmail.com>
2026-02-21 21:47:56 -0500

    feat: updated server for pub/sub
    tracking connections - join, publish & broadcast methods + lambda for session callbacks

--- server/server.cpp
+++ server/server.cpp
@@ -1,17 +1,22 @@
 // Source - https://stackoverflow.com/a/78513859
 // Retrieved 2026-02-21, License - CC BY-SA 4.0
-#include "session.hpp"
 #include "server.hpp"
+#include "actions.hpp"
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
+#include <boost/json.hpp>
+#include <boost/json/object.hpp>
 #include <iostream>
 #include <memory>
+#include <string>
 
 namespace asio = boost::asio;
 namespace beast = boost::beast;
+namespace json = boost::json;
 
 using tcp = asio::ip::tcp;
-using std::cerr, std::endl, std::exception,std::make_shared;
+using std::cerr, std::endl, std::string, std::exception, std::make_shared, std::weak_ptr, std::shared_ptr;
+
 
 void WebSocketServer::do_accept()
 {
@@ -26,9 +31,98 @@ void WebSocketServer::on_accept(beast::error_code ec, tcp::socket socket)
     cerr << "Accept error: " << ec.message() << endl;
 
   else
-    make_shared<WebSocketSession>(std::move(socket))->run();
+  {
+    auto session = make_shared<WebSocketSession>(std::move(socket));
+
+    //auto -> std::function<void(const string& message)>
+    auto handler_lambda = [this, session](const string& message)
+    {
+      handle_message(message,session);
+    };
+
+    session->set_handler(std::move(handler_lambda));
+    session->run();
+
+  }
 
   do_accept();
+}
+
+void WebSocketServer::handle_message(const string& message, shared_ptr<WebSocketSession> session)
+{
+  //ugly
+  json::value jv = json::parse(message);
+  json::object& jobj = jv.as_object();
+
+  string action = string(jobj["action"].as_string());
+  string username = string(jobj["from"].as_string());
+
+  string room = "general";
+
+  switch(parse_action(action))
+  {
+    case Action::JOIN:
+    {
+      join(username, room, session);
+      break;
+    }
+    case Action::PUBLISH:
+    {
+     string content = string(jobj["content"].as_string());
+
+      publish(username, room, content);
+      break;
+
+
+    }
+    case Action::NOOP:
+      return;
+  };
+
+}
+
+void WebSocketServer::broadcast(const string& username, const string& room, const string& message)
+{
+  set<string> subscribers = broker.fetch_subscribers(room);
+
+  for(string sub: subscribers)
+  {
+    //iterator for map, i don't like using auto but it'd look.. disgusting 
+    // map<std::string,std::weak_ptr<WebSocketSession>>::iterator!!!!
+    
+    auto client_iterator = connections.find(sub);
+
+    if (client_iterator != connections.end())
+    {
+      shared_ptr<WebSocketSession> client;
+      client = client_iterator->second.lock();
+
+      if(client)
+      {
+        if(sub != username)
+          client->send(message);
+      }
+
+      else
+        connections.erase(client_iterator);
+    }
+  }
+  std::cout << message << std::endl;
+}
+
+void WebSocketServer::join(const string& username, const string& room, std::shared_ptr<WebSocketSession> session)
+{
+  connections[username] = session;
+  broker.subscribe(username, room);
+  string content = "joined room " + room;
+
+  publish(username, room, content);
+}
+
+void WebSocketServer::publish(const string& username, const string& room, string& content)
+{
+  content = username + ": " + content;
+  broadcast(username, room, content);
 }
 
 int main()
--- server/server.hpp
+++ server/server.hpp
@@ -1,9 +1,13 @@
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
-
+#include "../broker/broker.hpp"
+#include "session.hpp"
+#include <map>
+#include <memory>
 namespace asio  = boost::asio;
 namespace beast = boost::beast;
 using tcp = asio::ip::tcp;
+
 
 class WebSocketServer 
 {
@@ -17,6 +21,16 @@ class WebSocketServer
   //moves to ws session
   void on_accept(beast::error_code ec, tcp::socket socket);
 
+  void handle_message(const string& message, std::shared_ptr<WebSocketSession> session);
+
+  void broadcast(const string& username, const string& room, const string& message);
+
+  void join(const string& username, const string& room, std::shared_ptr<WebSocketSession> session);
+
+  void publish(const string& username, const string& room, string& content);
+
   //accepts incoming TCP connections, forwards to WS
   tcp::acceptor acceptor_;
+  Broker broker;
+  std::map<std::string,std::weak_ptr<WebSocketSession>> connections;
 };
