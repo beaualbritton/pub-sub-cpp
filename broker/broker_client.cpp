ad2e3e5349da3c3b382b58f1aee69fb4ecb430c3
beau albritton <albrittoncb@gmail.com>
2026-03-30 11:44:00 -0400

    feat: broker client
    client that connects to broker server thru broker session - owned by ws_server

---
+++ broker/broker_client.cpp
@@ -0,0 +1,142 @@
+#include "broker_client.hpp"
+#include <boost/asio.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/core/bind_handler.hpp>
+#include <boost/beast/core/error.hpp>
+#include <exception>
+#include <deque>
+#include <iostream>
+
+namespace asio = boost::asio;
+namespace beast = boost::beast;
+namespace websocket = beast::websocket;
+
+using tcp = boost::asio::ip::tcp;
+using std::move, std::cerr, std::endl, std::string, std::function, std::exception;
+
+void BrokerClient::run(const string& host, const string& port)
+{
+  host_ = host;
+  resolver_.async_resolve(host,port,
+                          beast::bind_front_handler(&BrokerClient::on_resolve, this));
+}
+
+void BrokerClient::on_resolve(beast::error_code ec, tcp::resolver::results_type results)
+{
+  if(ec)
+  {
+    cerr << "Failure: " << ec.message() << " resolve" << endl;
+    return;
+  }
+
+  asio::async_connect(ws_.next_layer(), results,
+                      beast::bind_front_handler(&BrokerClient::on_connect, this));
+}
+
+void BrokerClient::on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type ep)
+{
+  if(ec)
+  {
+    cerr << "Failure: " << ec.message() << " connect" << endl;
+    return;
+  }
+
+  ws_.set_option(websocket::stream_base::timeout::suggested(
+                 beast::role_type::client));
+
+  ws_.set_option(websocket::stream_base::decorator(
+      [](websocket::request_type& req)
+      {
+          req.set(beast::http::field::user_agent,
+                  std::string(BOOST_BEAST_VERSION_STRING) + " websocket-client-async");
+      }));
+
+  host_ += ':' + std::to_string(ep.port());
+
+  ws_.async_handshake(host_, "/",
+                      beast::bind_front_handler(&BrokerClient::on_handshake, this));
+}
+
+
+void BrokerClient::on_handshake(beast::error_code ec)
+{
+  if(ec)
+  {
+    cerr << "Failure: " << ec.message() << " handshake" << endl;
+    return;
+  }
+
+  ws_.async_read(buffer_,beast::bind_front_handler(&BrokerClient::on_read, this));
+
+}
+
+void BrokerClient::do_read_loop()
+{
+  //schedules next async on_read, keeps session alive until read, so not reading/using after free, binds fuction to shared_from_this
+  ws_.async_read(buffer_, beast::bind_front_handler(&BrokerClient::on_read, this));
+}
+
+void BrokerClient::on_read(beast::error_code ec, size_t n)
+{
+  if (ec)
+  {
+    if (ec == websocket::error::closed)
+      return;
+
+    cerr << "Read error: " << ec.message() << endl;
+    return;
+  }
+
+  try
+  {
+    //read raw bytes into buffer, clear it & send to message handler in server
+    response_ = beast::buffers_to_string(buffer_.data());
+    buffer_.consume(n);
+
+    handler_(response_);
+    do_read_loop();
+
+  }
+  catch (exception const& e)
+  {
+    cerr << "Processing error: " << e.what() << endl;
+  }
+}
+
+//misleading, happens after send finishes
+//cycling read -> write -> read -> write etc etc
+void BrokerClient::on_write(beast::error_code ec, size_t n)
+{
+  cerr << "Write: " << n << " bytes (" << ec.message() << ")" << endl;
+  if (!ec)
+  {
+    send_queue.pop_front();
+    writing_ = false;
+
+    if(!send_queue.empty())
+      do_write();
+  }
+}
+
+void BrokerClient::set_handler(function<void(const string&)>handler)
+{
+  handler_ = std::move(handler);
+}
+
+void BrokerClient::send(const string& content)
+{
+  send_queue.emplace_back(content);
+  if(!writing_)
+    do_write();
+}
+
+void BrokerClient::do_write()
+{
+  writing_ = true;
+  string front = send_queue.front();
+  ws_.text(true);
+  ws_.async_write(asio::buffer(front),
+                  beast::bind_front_handler(&BrokerClient::on_write, this));
+
+}
---
+++ broker/broker_client.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include <boost/asio.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/core/error.hpp>
+#include <deque>
+#include <iostream>
+
+namespace beast = boost::beast;
+namespace websocket = beast::websocket;
+
+using tcp = boost::asio::ip::tcp;
+using std::move, std::cerr, std::endl, std::string, std::function;
+
+class BrokerClient
+{
+  public:
+  //wrap tcp socket ownership into ws_
+  explicit BrokerClient(boost::asio::io_context& ioc) : resolver_(ioc), ws_(boost::asio::make_strand(ioc)) { writing_ = false; }
+
+  ~BrokerClient() { cerr << "Server" << host_ << " closed" << endl; }
+
+  void run(const string& host, const string& port);
+
+  //handler_(std::move(handler))
+  void set_handler(function<void(const string&)>handler);
+  void send(const string& content);
+
+  private:
+
+  void on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type ep);
+  void on_resolve(beast::error_code ec, tcp::resolver::results_type results);
+  void on_handshake(beast::error_code ec);
+  void on_close(beast::error_code ec);
+
+  void do_read_loop();
+  void do_write();
+
+  void on_read(beast::error_code ec, size_t n);
+  void on_write(beast::error_code ec, size_t n);
+
+  
+  tcp::resolver resolver_;
+  // tcp socket handles bytes, websocket on top.
+  websocket::stream<tcp::socket> ws_;
+  beast::flat_buffer buffer_; //scary!
+  std::string host_;
+  std::string response_;
+
+
+
+  function<void(const string&)> handler_;
+  std::deque<string> send_queue;
+  bool writing_;
+};
