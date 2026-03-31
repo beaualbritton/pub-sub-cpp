225731fe1e80f56a0a8a1ca448f186fe13873c08
beau albritton <albrittoncb@gmail.com>
2026-03-31 14:58:20 -0400

    fix: update ws_server header

--- server/ws_server.hpp
+++ server/ws_server.hpp
@@ -1,6 +1,6 @@
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
-#include "../broker/broker.hpp"
+#include "../broker/broker_client.hpp"
 #include "../lib/server.hpp"
 #include "ws_session.hpp"
 #include <map>
@@ -13,7 +13,7 @@ using tcp = asio::ip::tcp;
 class WebSocketServer : public Server
 {
   public:
-  WebSocketServer(asio::io_context& ioc, tcp::endpoint endpoint) : Server(ioc, endpoint){};
+  WebSocketServer(asio::io_context& ioc, tcp::endpoint endpoint) : Server(ioc, endpoint), broker_client(ioc) { set_forward_handler(); broker_client.run("broker", "6767");};
   
 
   std::shared_ptr<Session> create_session(tcp::socket socket) override;
@@ -23,6 +23,8 @@ class WebSocketServer : public Server
   //moves to ws session
 
   void handle_message(const string& message, std::shared_ptr<WebSocketSession> session);
+  void handle_forward(const string& message);
+  void set_forward_handler();
 
   void broadcast(const string& username, const string& room, const string& message);
 
@@ -31,6 +33,6 @@ class WebSocketServer : public Server
   void publish(const string& username, const string& room, string& content);
 
   //accepts incoming TCP connections, forwards to WS
-  Broker broker;
+  BrokerClient broker_client;
   std::map<std::string,std::weak_ptr<WebSocketSession>> connections;
 };
